f77e803ed56502a8f19e90c5a7c4227e034d34cf
김주혜 <gimjuhye@gimjuhyeui-MacBookAir.local>
2025-05-01 15:31:34 +0900

    비디오1 루프 수정

--- src/components/CodeVideo.jsx
+++ src/components/CodeVideo.jsx
@@ -5,12 +5,15 @@ const CodeVideo = () => {
   return (
     <div className="code-video-wrapper">
     <video
-    className="code-video video01"
-    src="/videos/brewin_code.mp4"
-    autoPlay
-    muted
-    playsInline
+      className="code-video video01"
+      src={`${process.env.PUBLIC_URL}/videos/brewin_code.mp4`}
+      autoPlay
+      muted
+      loop
+      playsInline
+      preload="auto"
     >
+      {/* 비디오가 지원되지 않는 경우 대체 텍스트 */}
   Your browser does not support the video tag.
 </video>
 
